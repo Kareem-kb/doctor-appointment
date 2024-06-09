587e05ed4b90183163a566119ecf8f45d6cb7d50
Kareem Bakarbashat <thought.kb@gmail.com>
2024-06-09 20:21:02 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ export async function middleware(request) {
   );
 
   // Paths that do not require authentication
-  const publicPaths = ["/login", "/custom-error", "/api/auth", "/register"];
+  const publicPaths = ["/login", "/custom-error", "/api/auth", "/register", "/"];
  
   // Check if the path is one of the public paths
   if (publicPaths.some((path) => pathname.startsWith(path))) {
